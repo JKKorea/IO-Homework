5a87b4d2d3c8fe54d368730a033ddb2bafd47ac6
jkkorea <jongkun7987@gmail.com>
2017-05-21 10:53:27 +0900

    change console -> web

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyparser = require("body-parser");
+var bitcore = require("bitcore-lib");
 
 var hash = "000000000000000001b2f6c5ab6bf39114264f7526c1bce74d41a47ef8f2cd07";
-var out_value = 0;
 
-// 요구사항 1. args : hash, print info(n_tx, avg_value_of_tx, avg_fee, avg_size) 
-function bitcoin_request(blockhash){
-request({
-        url: "https://blockchain.info/block-index/" + blockhash +"?format=json",
-        json: true
-}, function(err, response, body){
-        if(err){
-            console.log("request 시 에러 발생. 에러 : " + err);
-            return;
-        }
-        var d_tx = body.tx.forEach(function(item1){
-            var out = item1.out.forEach(function(item2){
-                out_value += item2.value;
-            });
-        });
-      
-        var n_tx = body['n_tx'];
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+
+app.set("view engine", "ejs");
+
+function Wallet(uinput, cb){
+
+    var input = new Buffer(uinput);
+    var hash = bitcore.crypto.Hash.sha256(input);
+    var bn = bitcore.crypto.BN.fromBuffer(hash);
+    var pk = new bitcore.PrivateKey(bn).toWIF(); // wallet import format
+    var addy = new bitcore.PrivateKey(bn).toAddress();
     
-        var fee = body['fee'];
-        var avg_fee = fee / n_tx;
-        
-        var size = body['size'];
-        var avg_size = size / n_tx;
-        
-        var avg_value_of_tx = out_value / n_tx;
-    
-        console.log("n_tx : " + n_tx);
-        console.log("avg_value_of_tx : " + avg_value_of_tx);
-        console.log("avg_fee : " + avg_fee);
-        console.log("avg_size : " + avg_size);
-    });
+    cb(pk, addy);
+}
+
+function getPrice(returnPrice){
+    request({
+    url: "https://btc-e.com/api/3/ticker/btc_usd",
+    json: true
+    }, function(err, res, body){
+        returnPrice(body.btc_usd.last);
+    });    
 };
 
 // 요구사항 2. args : hash, print outinfo(spend, tx_index, type, addr, value, n, script)
@@ -70,14 +65,63 @@ request({
     });
 };
 
-bitcoin_outinfo(hash);
-
-bitcoin_request(hash);
+// 요구사항 1. args : hash, print info(n_tx, avg_value_of_tx, avg_fee, avg_size) 
+function bitcoin_request(blockhash, returnPrice){
+    request({
+        url: "https://blockchain.info/block-index/" + blockhash +"?format=json",
+        json: true
+}, function(err, response, body){
+        if(err){
+            console.log("request 시 에러 발생. 에러 : " + err);
+            return;
+        }    
+        var out_value = 0;
+        body.tx.forEach(function(item1){
+            item1.out.forEach(function(item2){
+                out_value += item2.value;
+            })
+        });
+        returnPrice(out_value, body.n_tx, body.fee, body.size);
+});
+}
 
 app.get("/", function(req, res){
-    res.send("Bitcoin");
+    bitcoin_request(hash, function(out_value, n_tx, fee, size, item3){
+        res.render("index", {
+        out_value : out_value,
+        n_tx: n_tx,
+        fee: fee,
+        size: size
+        });
+    bitcoin_outinfo(hash);
+    });  
 });
 
-app.listen(80, function(){ 
-    console.log("start");
+app.get("/wallet", function(req, res){
+    getPrice(function(lastPrice){
+        res.render("wallet", {
+            lastPrice: lastPrice
+        });
+    });
+});
+
+app.get("/converter", function(req, res){
+    getPrice(function(lastPrice){
+        res.render("converter", {
+            lastPrice: lastPrice
+        });
+    });
+});
+
+app.post("/wallet", function(req, res){
+    var src = req.body.src;
+    console.log(src);
+    Wallet(src, function(priv, addr){
+        res.send("The My wallet of: " + src + "<br>Addy: " + addr + "<br>Private Key: " + priv);
+    });
+    
+});
+
+app.listen(80, function(){
+    console.log("go");
 });
